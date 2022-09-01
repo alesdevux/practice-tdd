536424bd5f80859efa483aab88a9235b3b45c588
Ales <aleswebgit@gmail.com>
2022-09-01 22:58:31 +0200

    fix fizzbuzz function based on tdd

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,7 +2,7 @@ export const fizzbuzz = (number) => {
   if (typeof number !== 'number') throw new Error('parameter provided must be a number')
   if (Number.isNaN(number)) throw new Error('parameter provided must be a number')
 
-  if (number === 15) return 'FizzBuzz'
+  if (number % 15 === 0) return 'FizzBuzz'
   if (number % 3 === 0) return 'Fizz'
   if (number % 5 === 0) return 'Buzz'
 
