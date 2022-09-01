f9c1e6bbf8175ba27e2a79c8ce9233dc5963c885
Ales <aleswebgit@gmail.com>
2022-09-02 00:27:48 +0200

    fix canReconfigure function based on tdd

--- src/can-reconfigure.js
+++ src/can-reconfigure.js
@@ -3,6 +3,8 @@ export const canReconfigure = (form, to) => {
   if (typeof to !== 'string') throw new Error('to must be a string')
 
   if (form.length !== to.length) return false
+  const hasSameUniqueCharacters = new Set(form).size === new Set(to).size
+  if (!hasSameUniqueCharacters) return false
 
   return true
 }
