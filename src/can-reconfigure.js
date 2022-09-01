12ba8804c634b411397839b5eddbabfe7bbcd3f7
Ales <aleswebgit@gmail.com>
2022-09-02 00:00:18 +0200

    refactor canReconfigure
    quit if first parameter is undefined

--- src/can-reconfigure.js
+++ src/can-reconfigure.js
@@ -1,4 +1,3 @@
 export const canReconfigure = (form) => {
-  if (form === undefined) throw new Error('form is required')
   if (typeof form !== 'string') throw new Error('form must be a string')
 }
--- test/can-reconfigure.test.js
+++ test/can-reconfigure.test.js
@@ -2,10 +2,6 @@ import { describe, expect, it } from 'vitest'
 import { canReconfigure } from '../src/can-reconfigure'
 
 describe('canReconfigure', () => {
-  it('should be a function', () => {
-    expect(typeof canReconfigure).toBe('function')
-  })
-
   it('should throw if first parameter is missing', () => {
     expect(() => canReconfigure()).toThrow()
   })
