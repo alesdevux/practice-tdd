864a3cf7a7da21bbc348be7c6b5bd6d8f5eb9d01
Ales <aleswebgit@gmail.com>
2022-09-01 23:07:19 +0200

    fix fizzbuzz function based on tdd

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,7 +2,7 @@ export const fizzbuzz = (number) => {
   if (typeof number !== 'number') throw new Error('parameter provided must be a number')
   if (Number.isNaN(number)) throw new Error('parameter provided must be a number')
 
-  const multiples = { 3: 'Fizz', 5: 'Buzz' }
+  const multiples = { 3: 'Fizz', 5: 'Buzz', 7: 'Woff' }
   let output = ''
 
   Object
