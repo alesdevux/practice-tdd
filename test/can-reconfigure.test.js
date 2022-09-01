5a57e34fe984a527c5fca3ae53eb8a29a3a4545f
Ales <aleswebgit@gmail.com>
2022-09-02 00:12:07 +0200

    should return a boolean

--- test/can-reconfigure.test.js
+++ test/can-reconfigure.test.js
@@ -13,4 +13,8 @@ describe('canReconfigure', () => {
   it('should throw if second parameter is not a string', () => {
     expect(() => canReconfigure('form', 1)).toThrow()
   })
+
+  it('should return a boolean', () => {
+    expect(canReconfigure('a', 'b')).toBeTypeOf('boolean')
+  })
 })
