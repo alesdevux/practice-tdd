92a78a0e3f1a883843f5eb19b126161b268e8057
Ales <aleswebgit@gmail.com>
2022-09-01 23:46:57 +0200

    canReconfigure should be a function

---
+++ test/can-reconfigure.test.js
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+
+describe('canReconfigure', () => {
+  it('should be a function', () => {
+    expect(typeof canReconfigure).toBe('function')
+  })
+})
