e31ff8ee32410467283cda7ee4aa69e6992006fb
Ales <aleswebgit@gmail.com>
2022-09-01 23:58:06 +0200

    should throw if first parameter is not a string

--- test/can-reconfigure.test.js
+++ test/can-reconfigure.test.js
@@ -9,4 +9,8 @@ describe('canReconfigure', () => {
   it('should throw if first parameter is missing', () => {
     expect(() => canReconfigure()).toThrow()
   })
+
+  it('should throw if first parameter is not a string', () => {
+    expect(() => canReconfigure(1)).toThrow()
+  })
 })
