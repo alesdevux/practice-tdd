4539687a3dc3184aefd1aeb70f35e3492d268b01
Ales <aleswebgit@gmail.com>
2022-09-02 00:08:50 +0200

    should throw if second parameter is not a string

--- test/can-reconfigure.test.js
+++ test/can-reconfigure.test.js
@@ -9,4 +9,8 @@ describe('canReconfigure', () => {
   it('should throw if first parameter is not a string', () => {
     expect(() => canReconfigure(1)).toThrow()
   })
+
+  it('should throw if second parameter is not a string', () => {
+    expect(() => canReconfigure('form', 1)).toThrow()
+  })
 })
