cf99b4bb76567403da0b436a3a6085ff51c412e7
Ales <aleswebgit@gmail.com>
2022-09-01 22:49:39 +0200

    delete redundant test (is function)

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -2,10 +2,6 @@ import { describe, expect, it } from 'vitest'
 import { fizzbuzz } from '../src/fizzbuzz'
 
 describe('fizzbuz', () => {
-  it('should be a function', () => {
-    expect(typeof fizzbuzz).toBe('function')
-  })
-
   it('should throw if not number is provided as parameter', () => {
     expect(() => fizzbuzz()).toThrow()
   })
