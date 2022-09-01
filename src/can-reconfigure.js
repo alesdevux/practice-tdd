81c8d741cc39727dd25cdad3c71dad953318f9a8
Ales <aleswebgit@gmail.com>
2022-09-02 00:29:35 +0200

    refactor hasSameLength

--- src/can-reconfigure.js
+++ src/can-reconfigure.js
@@ -2,7 +2,9 @@ export const canReconfigure = (form, to) => {
   if (typeof form !== 'string') throw new Error('form must be a string')
   if (typeof to !== 'string') throw new Error('to must be a string')
 
-  if (form.length !== to.length) return false
+  const hasSameLength = form.length === to.length
+  if (!hasSameLength) return false
+
   const hasSameUniqueCharacters = new Set(form).size === new Set(to).size
   if (!hasSameUniqueCharacters) return false
 
