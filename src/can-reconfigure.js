7cc87411163abf28290982e3111276ab3df4b353
Ales <aleswebgit@gmail.com>
2022-09-02 00:09:25 +0200

    fix canReconfigure function based on tdd

--- src/can-reconfigure.js
+++ src/can-reconfigure.js
@@ -1,3 +1,4 @@
-export const canReconfigure = (form) => {
+export const canReconfigure = (form, to) => {
   if (typeof form !== 'string') throw new Error('form must be a string')
+  if (typeof to !== 'string') throw new Error('to must be a string')
 }
