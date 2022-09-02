e3728e63b1030222c3b70fb75d48d505d189d315
Ales <aleswebgit@gmail.com>
2022-09-02 10:08:56 +0200

    should render numbers

--- test/react.test.jsx
+++ test/react.test.jsx
@@ -2,6 +2,8 @@ import { cleanup, render, screen } from '@testing-library/react'
 import { afterEach, describe, it } from 'vitest'
 import Calculator from '../src/Calculator'
 
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 describe('Calculator', () => {
   afterEach(cleanup)
 
@@ -11,7 +13,11 @@ describe('Calculator', () => {
 
   it('should render title correctly', () => {
     render(<Calculator />)
-
     screen.getByText('Calculator')
+  })
+
+  it('should render numbers', () => {
+    render(<Calculator />)
+    numbers.forEach(number => screen.getByText(number))
   })
 })
