98b15bcce8846bca3d3ac81b462aaa892203ff59
Ales <aleswebgit@gmail.com>
2022-09-02 09:19:01 +0200

    fix Calculator component based on tdd

--- test/react.test.jsx
+++ test/react.test.jsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react'
 import { describe, it } from 'vitest'
+import Calculator from '../src/Calculator'
 
 describe('Calculator', () => {
   it('should render', () => {
